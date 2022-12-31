bd97627150abc277526805cbfc6525872d94282c
adasoraninda <adasoraninda@gmail.com>
2022-12-31 14:27:08 +0700

    update

--- src/components/AuthMenu.js
+++ src/components/AuthMenu.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Spacer from "./Spacer";
 
-const AuthMenu = () => {};
+const AuthMenu = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/*">Login</Link>
+        </li>
+        <Spacer h={20} />
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default AuthMenu;
